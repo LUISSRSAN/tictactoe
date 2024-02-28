8d316683a640d9cd6199482a1ac2ec0eed4afb9b
Luis Santiago <luissantiago@Luiss-Air.lan>
2024-02-28 14:42:42 -0500

    board and render functions initialized

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 console.log("Testing");
+
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+let board ;
+let turn = 'X';
+
+
+
+/*----- cached element references -----*/
+const squares = Array.from(document.querySelectorAll('#board div'));
+/*----- event listeners -----*/
+/*----- functions -----*/
+function render() {
+    board.forEach(function(mark, index){
+
+
+        //sets text content
+        squares[index].textContent = mark;
+    console.log(mark, index);
+    });
+    };
+    
+function init() {
+    board = [
+    '', '', '',
+    '', '', '',
+    '', '', ''
+    ];
+    render();
+    };
+
+    init();
+   
